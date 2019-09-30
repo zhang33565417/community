15643f265badec1e3b9fac4699fd13a6dc297304
ZE <zhang33565417@163.com>
2019-09-30 22:23:09 +0800

    this is first

---
+++ community/src/main/java/com/yizhou/CommunityApplication.java
@@ -0,0 +1,14 @@
+package com.yizhou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+        System.out.println("启动成功！");
+    }
+
+}
